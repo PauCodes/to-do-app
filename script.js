5471820f4f3e4bcc0601276580d06e271a588793
Pau <pauligonzalezincker@gmail.com>
2022-05-11 17:39:16 -0400

    added task checkbox functionality

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 const formElement = document.querySelector('form'); 
 console.log(formElement);
 
-//2.ADD EVENT LISTENER - WE PASS A 'SUBMIT' (call it submit because we are waiting for a submit to run my function) AND A CALL BACK FUNCTION as a parameter
+//2.ADD EVENT LISTENER - WE PASS A 'SUBMIT' (call it submit because we are waiting for a submit to run my function) AND A CALL BACK FUNCTION as a ARGUMENT
 formElement.addEventListener('submit', function(e){
    //stop the page fron refreshing
    e.preventDefault();
@@ -63,7 +63,7 @@ formElement.addEventListener('submit', function(e){
 
 })
 
-//CLICKING ON A TASK ALLOWS YOU TO TOGGLE BETWEEN CHECKED/UNCHECKED (AKA DONE V/S NOT DONE):
+
 //THIS EVENT LISTENER IS CALL OUTSIDE THE ONE ON TOP
 //WE HAVE TO QUERY THE DOM FOR THE ELEMENT AGAIN BECAUSE WE ARE OUT OF THE SCOPE AND DON'T HAVE ACCESS TO IT 
 //***!!!!!THIS WONT WORK BECAUSE WE CAN ONLY ADD EVENT LISTENER TO ELEMENTS THAT ALREADY EXIST IN THE DOM AT THE TIME OF THE CODE EXECUTION!!!!!***
@@ -75,11 +75,34 @@ formElement.addEventListener('submit', function(e){
 // });
 
 //SO... WE CAN USE 'EVENT PROPAGATION' AND TARGET THE PARENT (UL) OF MY LI THAT DOESN'T EXIST YET, AND ADD AN EVENT LISTENER TO IT
+//WE QUERY THE DOM FOR THE UL
+const ul = document.querySelector('ul');
+
+//PASS THE e AS AN ARGUMENT OF THE FUNCTION
+ul.addEventListener('click', function(e){
+    //THE THIS KEYWORD REPRESENT THE OBJECT WHICH OWNS THE CODE WHICH IS CURRENTLY RUNNING - DON'T USE ARROW FUNCTION WHEN USING THE THIS WEYWORD
+    // console.log(this);
+
+    console.log(e);
+
+    //AS LONG AS WE CLICKED ON THE ICON, THEN TOGGLE BETWEEN CHECKED/UNCHECKED (AKA DONE V/S NOT DONE):
+    //'i' REPRESENTS THE ICON THAT WAS CHECKED
+    if (e.target.localName === 'i') {
+        console.log('checked box was clicked'); //THIS WILL SHOW IN THE CONSOLE WHEN THE CHECKBOX WAS CHECKED
+
+        //toggle between checked / unchecked - go inside the e (event) in the console, go to target, them classList (here we will see the classes of my icon. And then we can use the toggle method to change classes whenever the user check or uncheck) 
+        e.target.classList.toggle('fa-square-check');
+        e.target.classList.toggle('fa-square');
+        
+    } 
+})
 
 
 
 
-;//BONUS!!!
+
+
+//BONUS!!!
 //ADD A RESET BUTTON WHICH CLEARS ALL OF THE TO DO'S
 //ADD A REMOVE TASK BUTTON TO EACH TASK
 //ADD A CONGRATULATION ALERT WHEN ALL OF THE EXISTING TO DO'S ARE CHECKED OFF
